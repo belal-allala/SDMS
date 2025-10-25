42f5afa15802593302884a27591b7931f6603a8c
belal-allala <belalallala810@gmail.com>
2025-10-26 00:00:37 +0100

    Commit 76: Adding a new comment on LivreurMapper.java

--- src/main/java/com/smartlogi/smds/mapper/LivreurMapper.java
+++ src/main/java/com/smartlogi/smds/mapper/LivreurMapper.java
@@ -1,4 +1,22 @@
 package com.smartlogi.smds.mapper;
 
+import com.smartlogi.smds.entity.Livreur;
+import com.smartlogi.smds.dto.LivreurDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.UUID;
+
+@Mapper(componentModel = "spring")
 public interface LivreurMapper {
+
+    LivreurMapper INSTANCE = Mappers.getMapper(LivreurMapper.class);
+
+    @Mapping(source = "zone.id", target = "zoneId")
+    LivreurDTO toDto(Livreur livreur);
+
+    @Mapping(source = "zoneId", target = "zone.id")
+    Livreur toEntity(LivreurDTO livreurDTO);
 }
+// Commit 76 on 2025-10-26 00:00:37
