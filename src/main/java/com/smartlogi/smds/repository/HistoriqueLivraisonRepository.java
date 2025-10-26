436bf638f5bd1d4099908a0f4993ab6ffa2655a3
belal-allala <belalallala810@gmail.com>
2025-10-26 21:19:24 +0100

    Commit 109: Adding a new comment on HistoriqueLivraisonRepository.java

--- src/main/java/com/smartlogi/smds/repository/HistoriqueLivraisonRepository.java
+++ src/main/java/com/smartlogi/smds/repository/HistoriqueLivraisonRepository.java
@@ -16,3 +16,4 @@ public interface HistoriqueLivraisonRepository extends JpaRepository<HistoriqueL
 // Commit 11 on 2025-10-28 21:51:32
 // Commit 78 on 2025-10-26 01:44:49
 // Commit 85 on 2025-10-28 16:45:35
+// Commit 109 on 2025-10-26 21:19:24
