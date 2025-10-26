979da5a6af536ef64d68716ffb4e57db915db22b
belal-allala <belalallala810@gmail.com>
2025-10-26 16:10:18 +0100

    Commit 30: Adding a new comment on Colis_ProduitDTO.java

--- src/main/java/com/smartlogi/smds/dto/Colis_ProduitDTO.java
+++ src/main/java/com/smartlogi/smds/dto/Colis_ProduitDTO.java
@@ -1,4 +1,35 @@
 package com.smartlogi.smds.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Colis_ProduitDTO {
+    private String id;
+
+    @NotBlank(message = "L'ID du colis ne peut pas être vide")
+    private String colisId;
+
+    @NotBlank(message = "L'ID du produit ne peut pas être vide")
+    private String produitId;
+
+    @NotNull(message = "La quantité ne peut pas être nulle")
+    @Positive(message = "La quantité doit être un nombre positif")
+    private Integer quantite;
+
+    @NotNull(message = "Le prix ne peut pas être nul")
+    @Positive(message = "Le prix doit être un nombre positif")
+    private Double prix;
+
+    @NotNull(message = "La date d'ajout ne peut pas être nulle")
+    private LocalDate dateAjout;
 }
+// Commit 30 on 2025-10-26 16:10:18
