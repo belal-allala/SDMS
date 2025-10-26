d4b44b7941f6caba7adb77a1c43e63ace3c7eee5
belal-allala <belalallala810@gmail.com>
2025-10-26 15:03:31 +0100

    Commit 107: Adding a new comment on ZoneRepository.java

--- src/main/java/com/smartlogi/smds/repository/ZoneRepository.java
+++ src/main/java/com/smartlogi/smds/repository/ZoneRepository.java
@@ -1,4 +1,12 @@
 package com.smartlogi.smds.repository;
 
-public interface ZoneRepository {
+import com.smartlogi.smds.entity.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ZoneRepository extends JpaRepository<Zone, UUID> {
 }
+// Commit 107 on 2025-10-26 15:03:31
