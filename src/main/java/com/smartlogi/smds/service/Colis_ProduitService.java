2227614c05d715b5a531fd7d7a3e5dfffc6f0230
belal-allala <belalallala810@gmail.com>
2025-10-26 10:15:29 +0100

    Commit 12: Adding a new comment on Colis_ProduitService.java

--- src/main/java/com/smartlogi/smds/service/Colis_ProduitService.java
+++ src/main/java/com/smartlogi/smds/service/Colis_ProduitService.java
@@ -1,13 +1,15 @@
 package com.smartlogi.smds.service;
 
-import com.smartlogi.smds.entity.Colis_Produit;
+import com.smartlogi.smds.dto.Colis_ProduitDTO;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface Colis_ProduitService {
-    Colis_Produit save(Colis_Produit colisProduit);
-    List<Colis_Produit> findAll();
-    Optional<Colis_Produit> findById(Long id);
-    void deleteById(Long id);
+    Colis_ProduitDTO save(Colis_ProduitDTO colisProduitDTO);
+    List<Colis_ProduitDTO> findAll();
+    Optional<Colis_ProduitDTO> findById(UUID id);
+    void deleteById(UUID id);
 }
+// Commit 12 on 2025-10-26 10:15:29
