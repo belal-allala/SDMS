d4ae81a6298c67ffaafe57992b7bd83bd67cc6ad
belal-allala <belalallala810@gmail.com>
2025-10-28 00:16:56 +0100

    Commit 21: Adding a new comment on ClientExpediteurMapper.java

--- src/main/java/com/smartlogi/smds/mapper/ClientExpediteurMapper.java
+++ src/main/java/com/smartlogi/smds/mapper/ClientExpediteurMapper.java
@@ -1,4 +1,17 @@
 package com.smartlogi.smds.mapper;
 
-public class ClientExpediteurMapper {
+import com.smartlogi.smds.entity.ClientExpediteur;
+import com.smartlogi.smds.dto.ClientExpediteurDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface ClientExpediteurMapper {
+
+    ClientExpediteurMapper INSTANCE = Mappers.getMapper(ClientExpediteurMapper.class);
+
+    ClientExpediteurDTO toDto(ClientExpediteur clientExpediteur);
+
+    ClientExpediteur toEntity(ClientExpediteurDTO clientExpediteurDTO);
 }
+// Commit 21 on 2025-10-28 00:16:56
