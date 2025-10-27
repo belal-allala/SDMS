d169109521bb2d2c82bc39980d7b3386c7d4e780
belal-allala <belalallala810@gmail.com>
2025-10-27 21:08:29 +0100

    Commit 103: Adding a new comment on LivreurService.java

--- src/main/java/com/smartlogi/smds/service/LivreurService.java
+++ src/main/java/com/smartlogi/smds/service/LivreurService.java
@@ -1,4 +1,15 @@
 package com.smartlogi.smds.service;
 
+import com.smartlogi.smds.dto.LivreurDTO;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface LivreurService {
+    LivreurDTO save(LivreurDTO livreurDTO);
+    List<LivreurDTO> findAll();
+    Optional<LivreurDTO> findById(UUID id);
+    void deleteById(UUID id);
 }
+// Commit 103 on 2025-10-27 21:08:29
