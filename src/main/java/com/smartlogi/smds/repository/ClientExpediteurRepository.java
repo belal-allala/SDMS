6ff9a10ed1529328dcfa9edd03c4dc4d5b2c6c66
belal-allala <belalallala810@gmail.com>
2025-10-27 20:46:11 +0100

    Commit 89: Adding a new comment on ClientExpediteurRepository.java

--- src/main/java/com/smartlogi/smds/repository/ClientExpediteurRepository.java
+++ src/main/java/com/smartlogi/smds/repository/ClientExpediteurRepository.java
@@ -11,3 +11,4 @@ public interface ClientExpediteurRepository extends JpaRepository<ClientExpedite
 }
 // Commit 54 on 2025-10-29 19:19:37
 // Commit 80 on 2025-10-29 15:17:19
+// Commit 89 on 2025-10-27 20:46:11
