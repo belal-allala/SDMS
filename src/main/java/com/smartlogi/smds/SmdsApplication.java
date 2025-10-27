c3129aa8777c10a1e1a1038a13637dd7c58e64f4
belal-allala <belalallala810@gmail.com>
2025-10-27 11:40:34 +0100

    initialisations de projet

---
+++ src/main/java/com/smartlogi/smds/SmdsApplication.java
@@ -0,0 +1,13 @@
+package com.smartlogi.smds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmdsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartlogi/smds/SmdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartlogi.smds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
