17fd6ffbc2c1827f38c3f6addda2fbec5cf19fad
belal-allala <belalallala810@gmail.com>
2025-10-28 13:05:04 +0100

    Commit 69: Adding a new comment on ColisRepository.java

--- src/main/java/com/smartlogi/smds/repository/ColisRepository.java
+++ src/main/java/com/smartlogi/smds/repository/ColisRepository.java
@@ -14,3 +14,4 @@ public interface ColisRepository extends JpaRepository<Colis, UUID> {
 // Commit 105 on 2025-10-29 02:30:34
 // Commit 119 on 2025-10-27 03:53:22
 // Commit 45 on 2025-10-28 01:51:49
+// Commit 69 on 2025-10-28 13:05:04
