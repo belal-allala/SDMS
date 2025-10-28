42c91482a9536d50bb39c58e1947a57a1c206125
belal-allala <belalallala810@gmail.com>
2025-10-28 02:08:38 +0100

    Commit 9: Adding a new comment on Colis_ProduitRepository.java

--- src/main/java/com/smartlogi/smds/repository/Colis_ProduitRepository.java
+++ src/main/java/com/smartlogi/smds/repository/Colis_ProduitRepository.java
@@ -1,4 +1,12 @@
 package com.smartlogi.smds.repository;
 
-public interface Colis_ProduitRepository {
+import com.smartlogi.smds.entity.Colis_Produit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface Colis_ProduitRepository extends JpaRepository<Colis_Produit, UUID> {
 }
+// Commit 9 on 2025-10-28 02:08:38
