dad4c3dc97d0830a6c428ce454353068a966deec
belal-allala <belalallala810@gmail.com>
2025-10-28 13:50:17 +0100

    Commit 16: Adding a new comment on Colis_ProduitMapper.java

--- src/main/java/com/smartlogi/smds/mapper/Colis_ProduitMapper.java
+++ src/main/java/com/smartlogi/smds/mapper/Colis_ProduitMapper.java
@@ -1,4 +1,23 @@
 package com.smartlogi.smds.mapper;
 
+import com.smartlogi.smds.entity.Colis_Produit;
+import com.smartlogi.smds.dto.Colis_ProduitDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.UUID;
+
+@Mapper(componentModel = "spring")
 public interface Colis_ProduitMapper {
+    Colis_ProduitMapper INSTANCE = Mappers.getMapper(Colis_ProduitMapper.class);
+
+    @Mapping(source = "colis.id", target = "colisId")
+    @Mapping(source = "produit.id", target = "produitId")
+    Colis_ProduitDTO toDTO(Colis_Produit colis_produit);
+
+    @Mapping(source = "colisId", target = "colis.id")
+    @Mapping(source = "produitId", target = "produit.id")
+    Colis_Produit toEntity(Colis_ProduitDTO colis_produitDTO);
 }
+// Commit 16 on 2025-10-28 13:50:17
