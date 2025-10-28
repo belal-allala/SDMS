6ec0d57c5c6922696771c2756695c46ab090782b
belal-allala <belalallala810@gmail.com>
2025-10-28 10:01:06 +0100

    Commit 120: Adding a new comment on ErrorResponseDTO.java

--- src/main/java/com/smartlogi/smds/dto/ErrorResponseDTO.java
+++ src/main/java/com/smartlogi/smds/dto/ErrorResponseDTO.java
@@ -1,4 +1,19 @@
 package com.smartlogi.smds.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorResponseDTO {
+
+    private int statusCode;
+    private String message;
+    private String details;
+    private LocalDateTime timestamp;
+    private String path;
 }
+// Commit 120 on 2025-10-28 10:01:06
