b2d46bde1588e78de74439b9d5768f4b44e96bb5
belal-allala <belalallala810@gmail.com>
2025-10-28 19:47:47 +0100

    Commit 19: Adding a new comment on ColisService.java

--- src/main/java/com/smartlogi/smds/service/ColisService.java
+++ src/main/java/com/smartlogi/smds/service/ColisService.java
@@ -1,4 +1,15 @@
 package com.smartlogi.smds.service;
 
+import com.smartlogi.smds.dto.ColisDTO;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ColisService {
+    ColisDTO save(ColisDTO colisDTO);
+    List<ColisDTO> findAll();
+    Optional<ColisDTO> findById(UUID id);
+    void deleteById(UUID id);
 }
+// Commit 19 on 2025-10-28 19:47:47
