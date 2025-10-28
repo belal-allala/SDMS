d82056d181add571ea5a1d03474d11b5ba89559f
belal-allala <belalallala810@gmail.com>
2025-10-28 11:25:39 +0100

    Commit 8: Adding a new comment on ClientExpediteurDTO.java

--- src/main/java/com/smartlogi/smds/dto/ClientExpediteurDTO.java
+++ src/main/java/com/smartlogi/smds/dto/ClientExpediteurDTO.java
@@ -1,4 +1,37 @@
 package com.smartlogi.smds.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClientExpediteurDTO {
+    private String id;
+
+    @NotBlank(message = "Le nom ne peut pas être vide")
+    @Size(max = 100, message = "Le nom ne peut pas dépasser 100 caractères")
+    private String nom;
+
+    @NotBlank(message = "Le prénom ne peut pas être vide")
+    @Size(max = 100, message = "Le prénom ne peut pas dépasser 100 caractères")
+    private String prenom;
+
+    @NotBlank(message = "L'email ne peut pas être vide")
+    @Email(message = "L'email doit être valide")
+    @Size(max = 255, message = "L'email ne peut pas dépasser 255 caractères")
+    private String email;
+
+    @NotBlank(message = "Le numéro de téléphone ne peut pas être vide")
+    @Size(max = 20, message = "Le numéro de téléphone ne peut pas dépasser 20 caractères")
+    private String telephone;
+
+    @NotBlank(message = "L'adresse ne peut pas être vide")
+    @Size(max = 255, message = "L'adresse ne peut pas dépasser 255 caractères")
+    private String adresse;
 }
+// Commit 8 on 2025-10-28 11:25:39
