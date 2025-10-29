e516a864e51a333fbcb23f371323b933d763cd2e
belal-allala <belalallala810@gmail.com>
2025-10-29 19:19:37 +0100

    Commit 54: Adding a new comment on ClientExpediteurRepository.java

--- src/main/java/com/smartlogi/smds/repository/ClientExpediteurRepository.java
+++ src/main/java/com/smartlogi/smds/repository/ClientExpediteurRepository.java
@@ -1,4 +1,12 @@
 package com.smartlogi.smds.repository;
 
-public class ClientExpediteurRepository {
+import com.smartlogi.smds.entity.ClientExpediteur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ClientExpediteurRepository extends JpaRepository<ClientExpediteur, UUID> {
 }
+// Commit 54 on 2025-10-29 19:19:37
