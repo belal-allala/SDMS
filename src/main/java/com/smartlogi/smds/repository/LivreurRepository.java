97fb0a35dfbdee058af6ebad971e663e5422f16a
belal-allala <belalallala810@gmail.com>
2025-10-29 14:22:29 +0100

    Commit 75: Adding a new comment on LivreurRepository.java

--- src/main/java/com/smartlogi/smds/repository/LivreurRepository.java
+++ src/main/java/com/smartlogi/smds/repository/LivreurRepository.java
@@ -1,4 +1,12 @@
 package com.smartlogi.smds.repository;
 
-public class LivreurRepository {
+import com.smartlogi.smds.entity.Livreur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface LivreurRepository extends JpaRepository<Livreur, UUID> {
 }
+// Commit 75 on 2025-10-29 14:22:29
