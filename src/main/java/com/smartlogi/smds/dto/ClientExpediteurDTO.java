9a8f452707749cdc6bc72b16b7d16e6c6d1ffa82
belal-allala <belalallala810@gmail.com>
2025-10-29 10:25:20 +0100

    Commit 44: Adding a new comment on ClientExpediteurDTO.java

--- src/main/java/com/smartlogi/smds/dto/ClientExpediteurDTO.java
+++ src/main/java/com/smartlogi/smds/dto/ClientExpediteurDTO.java
@@ -37,3 +37,4 @@ public class ClientExpediteurDTO {
 // Commit 8 on 2025-10-28 11:25:39
 // Commit 65 on 2025-10-30 06:12:41
 // Commit 24 on 2025-10-30 12:24:04
+// Commit 44 on 2025-10-29 10:25:20
