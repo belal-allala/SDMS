9ab5e00383a418e250687f7ee723f92a95fb4f34
belal-allala <belalallala810@gmail.com>
2025-10-29 22:26:18 +0100

    Commit 28: Adding a new comment on ProduitDTO.java

--- src/main/java/com/smartlogi/smds/dto/ProduitDTO.java
+++ src/main/java/com/smartlogi/smds/dto/ProduitDTO.java
@@ -1,4 +1,33 @@
 package com.smartlogi.smds.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProduitDTO {
+    private String id;
+
+    @NotBlank(message = "Le nom ne peut pas être vide")
+    @Size(max = 100, message = "Le nom ne peut pas dépasser 100 caractères")
+    private String nom;
+
+    @NotBlank(message = "La catégorie ne peut pas être vide")
+    @Size(max = 100, message = "La catégorie ne peut pas dépasser 100 caractères")
+    private String categorie;
+
+    @NotNull(message = "Le poids ne peut pas être nul")
+    @Positive(message = "Le poids doit être un nombre positif")
+    private Double poids;
+
+    @NotNull(message = "Le prix ne peut pas être nul")
+    @Positive(message = "Le prix doit être un nombre positif")
+    private Double prix;
 }
+// Commit 28 on 2025-10-29 22:26:18
