ceede80e4a79f774fdbe8d84e4e88112b14313bc
belal-allala <belalallala810@gmail.com>
2025-10-29 22:22:34 +0100

    Commit 2: Adding a new comment on DestinataireMapper.java

--- src/main/java/com/smartlogi/smds/mapper/DestinataireMapper.java
+++ src/main/java/com/smartlogi/smds/mapper/DestinataireMapper.java
@@ -1,4 +1,17 @@
 package com.smartlogi.smds.mapper;
 
+import com.smartlogi.smds.entity.Destinataire;
+import com.smartlogi.smds.dto.DestinataireDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
 public interface DestinataireMapper {
+
+    DestinataireMapper INSTANCE = Mappers.getMapper(DestinataireMapper.class);
+
+    DestinataireDTO toDTO(Destinataire destinataire);
+
+    Destinataire toEntity(DestinataireDTO destinataireDTO);
 }
+// Commit 2 on 2025-10-29 22:22:34
