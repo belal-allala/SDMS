1db060ea942e2b97a6d64b6df573b215748474e6
belal-allala <belalallala810@gmail.com>
2025-10-29 17:56:32 +0100

    Commit 52: Adding a new comment on ZoneDTO.java

--- src/main/java/com/smartlogi/smds/dto/ZoneDTO.java
+++ src/main/java/com/smartlogi/smds/dto/ZoneDTO.java
@@ -1,4 +1,23 @@
 package com.smartlogi.smds.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ZoneDTO {
+    private String id;
+
+    @NotBlank(message = "Le nom ne peut pas être vide")
+    @Size(max = 100, message = "Le nom ne peut pas dépasser 100 caractères")
+    private String nom;
+
+    @NotBlank(message = "Le code postal ne peut pas être vide")
+    @Size(max = 10, message = "Le code postal ne peut pas dépasser 10 caractères")
+    private String codePostal;
 }
+// Commit 52 on 2025-10-29 17:56:32
