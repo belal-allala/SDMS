15789fb2a53cd6d831fb0584521b0f58ce3cd772
belal-allala <belalallala810@gmail.com>
2025-10-29 22:42:49 +0100

    Commit 66: Adding a new comment on ClientExpediteurService.java

--- src/main/java/com/smartlogi/smds/service/ClientExpediteurService.java
+++ src/main/java/com/smartlogi/smds/service/ClientExpediteurService.java
@@ -1,4 +1,16 @@
 package com.smartlogi.smds.service;
 
+import com.smartlogi.smds.dto.ClientExpediteurDTO;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ClientExpediteurService {
+
+    ClientExpediteurDTO save(ClientExpediteurDTO clientExpediteurDto);
+    List<ClientExpediteurDTO> findAll();
+    Optional<ClientExpediteurDTO> findById(UUID id);
+    void deleteById(UUID id);
 }
+// Commit 66 on 2025-10-29 22:42:49
