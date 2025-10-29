ca74edd19ac041cea92738ceee80c4cd6ef102be
belal-allala <belalallala810@gmail.com>
2025-10-29 02:06:56 +0100

    Commit 47: Adding a new comment on DestinataireService.java

--- src/main/java/com/smartlogi/smds/service/DestinataireService.java
+++ src/main/java/com/smartlogi/smds/service/DestinataireService.java
@@ -1,4 +1,15 @@
 package com.smartlogi.smds.service;
 
+import com.smartlogi.smds.dto.DestinataireDTO;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface DestinataireService {
+    DestinataireDTO save(DestinataireDTO destinataire);
+    List<DestinataireDTO> findAll();
+    Optional<DestinataireDTO> findById(UUID id);
+    void deleteById(UUID id);
 }
+// Commit 47 on 2025-10-29 02:06:56
