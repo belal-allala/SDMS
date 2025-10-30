a33d9f10e68d54bb0349c3d9e9107ffbc8e41128
belal-allala <belalallala810@gmail.com>
2025-10-30 02:47:46 +0100

    Commit 121: Adding a new comment on HistoriqueLivraisonMapper.java

--- src/main/java/com/smartlogi/smds/mapper/HistoriqueLivraisonMapper.java
+++ src/main/java/com/smartlogi/smds/mapper/HistoriqueLivraisonMapper.java
@@ -1,4 +1,21 @@
 package com.smartlogi.smds.mapper;
 
+import com.smartlogi.smds.entity.HistoriqueLivraison;
+import com.smartlogi.smds.dto.HistoriqueLivraisonDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.UUID;
+
+@Mapper(componentModel = "spring")
 public interface HistoriqueLivraisonMapper {
+    HistoriqueLivraisonMapper INSTANCE = Mappers.getMapper(HistoriqueLivraisonMapper.class);
+
+    @Mapping(source = "colis.id", target = "colisId")
+    HistoriqueLivraisonDTO toDTO(HistoriqueLivraison historiqueLivraison);
+
+    @Mapping(source = "colisId", target = "colis.id")
+    HistoriqueLivraison toEntity(HistoriqueLivraisonDTO historiqueLivraisonDTO);
 }
+// Commit 121 on 2025-10-30 02:47:46
