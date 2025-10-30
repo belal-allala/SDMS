cffa85281b9f2c02d09cc09b57e0f255299de809
belal-allala <belalallala810@gmail.com>
2025-10-30 09:29:20 +0100

    Commit 114: Adding a new comment on ColisMapper.java

--- src/main/java/com/smartlogi/smds/mapper/ColisMapper.java
+++ src/main/java/com/smartlogi/smds/mapper/ColisMapper.java
@@ -1,4 +1,27 @@
 package com.smartlogi.smds.mapper;
 
+import com.smartlogi.smds.entity.Colis;
+import com.smartlogi.smds.dto.ColisDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.UUID;
+
+@Mapper(componentModel = "spring")
 public interface ColisMapper {
+    ColisMapper INSTANCE = Mappers.getMapper(ColisMapper.class);
+
+    @Mapping(source = "destinataire.id", target = "destinataireId")
+    @Mapping(source = "clientExpediteur.id", target = "clientExpediteurId")
+    @Mapping(source = "livreur.id", target = "livreurId")
+    @Mapping(source = "zone.id", target = "zoneId")
+    ColisDTO toDTO(Colis colis);
+
+    @Mapping(source = "destinataireId", target = "destinataire.id")
+    @Mapping(source = "clientExpediteurId", target = "clientExpediteur.id")
+    @Mapping(source = "livreurId", target = "livreur.id")
+    @Mapping(source = "zoneId", target = "zone.id")
+    Colis toEntity(ColisDTO colisDTO);
 }
+// Commit 114 on 2025-10-30 09:29:20
