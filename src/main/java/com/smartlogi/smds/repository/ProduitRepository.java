ec3ff34dda1ea637d12493ad05e33add29b2c4f9
belal-allala <belalallala810@gmail.com>
2025-10-30 07:59:39 +0100

    Commit 69: Adding a new comment on ProduitRepository.java

--- src/main/java/com/smartlogi/smds/repository/ProduitRepository.java
+++ src/main/java/com/smartlogi/smds/repository/ProduitRepository.java
@@ -1,4 +1,12 @@
 package com.smartlogi.smds.repository;
 
-public interface ProduitRepository {
+import com.smartlogi.smds.entity.Produit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProduitRepository extends JpaRepository<Produit,UUID>{
 }
+// Commit 69 on 2025-10-30 07:59:39
