dd9b46d943c8dd57e8daf0f6dbab2f660119fe11
belal-allala <belalallala810@gmail.com>
2025-10-30 22:30:48 +0100

    Commit 7: Adding a new comment on ZoneMapper.java

--- src/main/java/com/smartlogi/smds/mapper/ZoneMapper.java
+++ src/main/java/com/smartlogi/smds/mapper/ZoneMapper.java
@@ -1,4 +1,15 @@
 package com.smartlogi.smds.mapper;
 
+import com.smartlogi.smds.dto.ZoneDTO;
+import com.smartlogi.smds.entity.Zone;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
 public interface ZoneMapper {
+
+    ZoneMapper INSTANCE = Mappers.getMapper(ZoneMapper.class);
+    Zone toEntity(ZoneDTO zoneDTO);
+    ZoneDTO toDTO(Zone zone);
 }
+// Commit 7 on 2025-10-30 22:30:48
