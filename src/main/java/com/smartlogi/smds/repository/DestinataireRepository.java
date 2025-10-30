436cc657a01f6ffb48480e065cb3d3dd2d39942d
belal-allala <belalallala810@gmail.com>
2025-10-30 03:13:01 +0100

    Commit 31: Adding a new comment on DestinataireRepository.java

--- src/main/java/com/smartlogi/smds/repository/DestinataireRepository.java
+++ src/main/java/com/smartlogi/smds/repository/DestinataireRepository.java
@@ -10,3 +10,4 @@ import java.util.UUID;
 public interface DestinataireRepository extends JpaRepository<Destinataire, UUID> {
 }
 // Commit 29 on 2025-10-28 07:45:26
+// Commit 31 on 2025-10-30 03:13:01
