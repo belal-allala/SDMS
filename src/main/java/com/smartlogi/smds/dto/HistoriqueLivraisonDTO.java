d33c73d675e4987dd076912e2f7fa2431f975854
belal-allala <belalallala810@gmail.com>
2025-10-30 23:59:01 +0100

    Commit 59: Adding a new comment on HistoriqueLivraisonDTO.java

--- src/main/java/com/smartlogi/smds/dto/HistoriqueLivraisonDTO.java
+++ src/main/java/com/smartlogi/smds/dto/HistoriqueLivraisonDTO.java
@@ -1,4 +1,32 @@
 package com.smartlogi.smds.dto;
 
+import com.smartlogi.smds.entity.StatutColis;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class HistoriqueLivraisonDTO {
+
+    private String id;
+
+    @NotNull(message = "Le statut ne peut pas être nul")
+    private StatutColis statut;
+
+    @NotNull(message = "La date de changement ne peut pas être nulle")
+    private LocalDateTime dateChangement;
+
+    @Size(max = 255, message = "Le commentaire ne peut pas dépasser 255 caractères")
+    private String commentaire;
+
+    @NotBlank(message = "L'ID du colis ne peut pas être vide")
+    private String colisId;
 }
+// Commit 59 on 2025-10-30 23:59:01
