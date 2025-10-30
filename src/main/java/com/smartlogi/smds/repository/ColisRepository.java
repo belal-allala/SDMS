476f69200957af3091a921a75fbf88ab731336e7
belal-allala <belalallala810@gmail.com>
2025-10-30 17:40:45 +0100

    Commit 39: Adding a new comment on ColisRepository.java

--- src/main/java/com/smartlogi/smds/repository/ColisRepository.java
+++ src/main/java/com/smartlogi/smds/repository/ColisRepository.java
@@ -1,4 +1,12 @@
 package com.smartlogi.smds.repository;
 
-public interface ColisRepository {
+import com.smartlogi.smds.entity.Colis;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ColisRepository extends JpaRepository<Colis, UUID> {
 }
+// Commit 39 on 2025-10-30 17:40:45
